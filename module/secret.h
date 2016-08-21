d806424a9d28334c24e9529c66ab30e00e46437a
Jongho Kim <scurry2119@gmail.com>
2016-08-21 08:29:16 +0000

    Add #ifndef _SECRET_H

--- module/secret.h
+++ module/secret.h
@@ -1,5 +1,8 @@
+#ifndef _SECRET_H
+#define _SECRET_H
 typedef struct jongho *jongho_t;
 
 jongho_t init_jongho();
 void tell_secret(jongho_t jh, int secret);
 int square_secret(jongho_t jh);
+#endif
